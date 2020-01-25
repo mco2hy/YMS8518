cb0f93a258bbf007e5d85eb51a6af395648cc6ef
Can Yaralı <mco2hy@gmail.com>
2020-01-25 18:32:07 +0300

    Threading(25.01.2020) #26

---
+++ ThreadingConsoleApp/ThreadingConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            new Thread(Item).Start();
+            new Thread(Item).Start();
+            new Thread(Item).Start();
+            new Thread(Item).Start();
+            new Thread(Item).Start();
+            new Thread(Item).Start();
+
+            int completedThreadCount = 0;
+            while (true)
+            {
+                if (completedThreadCount < Count)
+                {
+                    Console.WriteLine("İşini Bitiren Thread sayısı: " + Count);
+                    completedThreadCount = Count;
+                }
+                Thread.Sleep(500);
+            }
+            Console.ReadLine();
+        }
+
+        private static int Count = 0;
+        private static readonly object Locker = new object();
+
+        public static void Item()
+        {
+            lock (Locker)
+            {
+                Console.WriteLine(DateTime.Now);
+                Thread.Sleep(millisecondsTimeout: 2000);
+            }
+            Count++;
+        }
+    }
+}
---
+++ ZarAtma/ZarAtma/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ZarAtma
+{
+    class Program
+    {
+        static int playerNum;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaç kişi oynayacak");
+            playerNum = Convert.ToInt32(Console.ReadLine());
+
+   
+            for (int i = 1; i <= playerNum; i++)
+            {
+                rollStats.Add(new RollStat() {
+                    Id = i,
+                    Count = 0,
+                    Value = 0
+                });
+                new Thread(Roll).Start(i);
+            }
+            new Thread(Referee).Start();
+
+        }
+        public class RollStat
+        {
+            public int Id { get; set; }
+            public int Count { get; set; }
+
+            public int Value { get; set; }
+        }
+        private static bool CancelThreads = false;
+        private static Random _random = new Random();
+        private static List<RollStat> rollStats = new List<RollStat>();
+        private static Tuple<int, int, int> Stats = new Tuple<int, int, int>(0,0,0);
+        private static void Roll(object id)
+        {
+            while (true)
+            {
+                if (CancelThreads)
+                {
+                    break;
+                }
+                var selfStat = rollStats.Single(a => a.Id == (int)id);
+                if (selfStat.Count < rollStats.OrderByDescending(a => a.Count).First().Count 
+                    || rollStats.Count(a => a.Count == selfStat.Count) == playerNum)
+                {
+                    int result = _random.Next(1, 10);
+                    selfStat.Count++;
+                    selfStat.Value += result;
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        private static void Referee()
+        {
+            int turn = 0;
+            while (true)
+            {
+                
+                if (rollStats.Count(a => a.Count == turn) == playerNum)
+                {
+                    string stats = string.Empty;
+
+
+                    foreach (RollStat rollStat in rollStats)
+                    {
+                        stats += rollStat.Id + ": " + rollStat.Value + " |";
+                    }
+
+                    Console.WriteLine(stats);
+
+                    if (rollStats.Any(a => a.Value >= 100))
+                    {
+                        CancelThreads = true;
+
+                        List<RollStat> winners = rollStats.Where(a => a.Value >= 100).ToList();
+                        Console.WriteLine("Kazanan belli oldu! " + string.Join(",", winners.Select(a => a.Id)));
+                    }
+                    turn++;
+                }
+            }
+        }
+    }
+}
