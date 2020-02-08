9e67cba8a63126e1a0cd5064b1e0b07dc2938d5c
Can Yaralı <mco2hy@gmail.com>
2020-02-08 19:16:46 +0300

    Chat #29

---
+++ Chat/Chat/wwwroot/js/chat.js
@@ -0,0 +1,37 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("http://localhost:5000/websocket").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+
+    message = "<li>" + new Date().getHours() + " : " + new Date().getMinutes() + " : " + new Date().getSeconds() + "  " + user + ": " + message + "</li>";
+    message = message.replace(/:\)/g, "<img src='/img/icon_smile.png' style='height: 30px;' />");
+    message = message.replace(/:\@/g, "<img src='/img/AngrySmile.png' style='height: 30px;' />");
+    message = message.replace(/:b/g, "<img src='/img/BirthdaySmile.png' style='height: 30px;' />");
+    message = message.replace(/:c/g, "<img src='/img/LipSmile.png' style='height: 30px;' />");
+    message = message.replace(/:d/g, "<img src='/img/BigSmile.png' style='height: 30px;' />");
+    message = message.replace(/:e/g, "<img src='/img/BlueEyesSmile.png' style='height: 30px;' />");
+    message = message.replace(/:f/g, "<img src='/img/FloversSmile.png' style='height: 30px;' />");
+    message = message.replace(/<3/g, "<img src='/img/LoveSmile.png' style='height: 30px;' />");
+    message = message.replace(/:h/g, "<img src='/img/HappySmile.png' style='height: 30px;' />");
+    message = message.replace(/:v/g, "<img src='/img/VictorySmile.png' style='height: 30px;' />");
+
+    document.getElementById("messagesList").innerHTML = 
+        document.getElementById("messagesList").innerHTML + message;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message);
+    event.preventDefault();
+});
---
+++ Deputation/Deputation/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace Deputation
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            ////////Action<string, string> testDelegate2 = Concat;
+            ////////testDelegate2("x", "y");
+
+            ////////Console.WriteLine("Singleton'dan Gelen Nesne: " + ConcattedString);
+
+            ////////Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //////Func<string, string, string> testDelegate;
+            //////testDelegate = delegate (string x, string y)
+            //////{
+            //////    return y + x;
+            //////};
+
+            //////string output = testDelegate("bilge", "adam");
+            //////Console.WriteLine(output);
+
+            ////testDelegate = Concat;
+            ////output = testDelegate("bilge", "adam");
+            ////Console.WriteLine(output);
+
+            ////Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Func<int, int, int> testDelegate = Bol;
+            //int output = testDelegate(2, 3);
+            //Console.WriteLine(output);
+
+            //testDelegate = Carp;
+            //output = testDelegate(4, 5);
+
+            //Console.WriteLine(output);
+            //Console.ReadLine();
+        }
+        ////////public static string ConcattedString = string.Empty;
+        ////////public static void Concat(string ilk, string son)
+        ////////{
+        ////////    ConcattedString = ilk + son ;
+        ////////    Console.WriteLine( ilk + son);
+        ////////} 
+        ////public static string Concat(string ilk, string son)
+        ////{
+        ////    return ilk + son;
+        ////} 
+        //public static int Carp(int x, int y)
+        //{
+        //    return x * y;
+        //}
+        //public static int Bol(int x, int y)
+        //{
+        //    return x / y;
+        //}
+    }
+}
